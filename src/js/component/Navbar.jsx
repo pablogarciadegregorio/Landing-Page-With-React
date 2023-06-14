5d678f4056d6a91410621242e07f2aa445e6641f
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-06-14 23:06:36 +0000

    Solucionado ToogleButt claro y navicons derecha

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,15 +5,15 @@ import React from "react";
 const Navbar = () => {
 	return (
 
-		<nav className="navbar navbar-expand-lg navbar-light bg-dark ">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div className="container-fluid d-flex ">
-    <a className="navbar-brand text-white " href="#">Start Bootstrap</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <a className=" col-9 navbar-brand text-white " href="#">Start Bootstrap</a>
+    <button className="navbar-toggler  " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon "></span>
     </button>
 
-    <div className="collapse navbar-collapse float-right" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-white justify-content-end">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-white">
         <li className="nav-item">
           <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
         </li>
